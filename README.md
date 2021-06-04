4e8749610fd4aaf65ffa0be345c24f03a72757e9
Avni-abc <84711897+Avni-abc@users.noreply.github.com>
2021-06-04 15:59:56 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# GUESSITGAMEFUN
+# game
---
+++ app.js
@@ -0,0 +1,6 @@
+// Make the start button work
+const $startbutton = document.getElementById("start");
+$startbutton.onclick=()=>{
+location.href="./game.html";
+};
+
